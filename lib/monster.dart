90dfe70e6810a3bf2a324f7b7a0ca7de40c023cb
Hamiric <do9870@naver.com>
2024-11-05 12:00:10 +0900

    Fix : "코드 수정3"
    캐릭터 방어메서드, 몬스터 공격 메서드

--- lib/monster.dart
+++ lib/monster.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:battle_rpg_game/character.dart';
 
 class Monster {
@@ -12,8 +14,14 @@ class Monster {
   // 캐릭터에게 공격을 가하며 피해를 입힙니다
   // 캐릭터에게 입히는 데미지는 공격력에서 캐릭터의 방어력을 뺀 값이며, 최소 데미지는 0 이상입니다
   void attackCharacter(Character character) {
-    int damege = atk - character.dfs;
-    if(damege < 0) damege = 0;
+    int calculatedamege = atk - character.dfs;
+    int damege = 0;
+    
+    if (calculatedamege <= 0) {
+      damege = 0;
+    } else {
+      damege = Random().nextInt(atk - character.dfs);
+    }
 
     print('${name}이(가) ${character.name}에게 ${damege}만큼 데미지를 입혔습니다.');
     character.hp -= damege;
